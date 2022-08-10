2edbd4d49eebc26d260f720d4fd1773e3273a222
SalsaGal <fielddashos@gmail.com>
2022-08-11 09:19:53 +1000

    Load files when dragged in

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
 use current::*;
+use image::DynamicImage;
+use winit::event::{Event, WindowEvent};
 
 fn main() {
     Outliner::run();
 }
 
-struct Outliner {}
+struct Outliner {
+    original_image: Option<DynamicImage>,
+}
 
 impl Game for Outliner {
-    fn init(_: &mut GameData) -> Self {
-        Self {}
+    fn init(data: &mut GameData) -> Self {
+        data.set_resizable(true);
+        data.set_title("Outliner");
+
+        Self {
+            original_image: None,
+        }
+    }
+
+    fn handle_event(&mut self, _: &mut GameData, event: &Event<()>) {
+        if let Event::WindowEvent { event: WindowEvent::DroppedFile(path), .. }  = event {
+            match image::open(path) {
+                Ok(image) => {
+                    self.original_image = Some(image);
+                },
+                Err(err) => eprintln!("Error: {err}"),
+            }
+        }
     }
 }
