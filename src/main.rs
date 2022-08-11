acebeaa0598842d0e215b46e1df4e5997960a693
SalsaGal <fielddashos@gmail.com>
2022-08-11 10:57:43 +1000

    Load image from command line for testing

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,20 @@ impl Game for Outliner {
         data.set_resizable(true);
         data.set_title("Outliner");
 
+        let original_image = match std::env::args().nth(1) {
+            Some(path) => {
+                let image = image::open(path).unwrap();
+                let (width, height) = image.dimensions();
+                let id = data.graphics.load_image(&image, sprite::Filter::Nearest);
+                let sprite = Sprite::new_texture_rect(data.graphics, id)
+                    .with_transform(Transform::scale((width as f32, height as f32).into()));
+                Some((image, sprite))
+            }
+            None => None,
+        };
+
         Self {
-            original_image: None,
+            original_image,
         }
     }
 
