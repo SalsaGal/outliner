bd0894088338c5e1530057ea763e8cd9baf0c8d2
SalsaGal <fielddashos@gmail.com>
2022-08-12 15:34:37 +1000

    Render background

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use current::sprite::{Sprite, Transform};
+use current::sprite::{Sprite, Transform, RectTextureMap};
 use current::*;
 use image::DynamicImage;
 use winit::event::{Event, WindowEvent};
@@ -8,6 +8,7 @@ fn main() {
 }
 
 struct Outliner {
+    background: Sprite,
     original_image: Option<(DynamicImage, Sprite)>,
 }
 
@@ -16,7 +17,7 @@ fn get_original_image(data: &mut GameData, path: &str) -> Option<(DynamicImage, 
         Ok(image) => {
             let id = data.graphics.load_image(&image, sprite::Filter::Nearest);
             let sprite = Sprite::new_texture_rect(data.graphics, id).with_transform(
-                Transform::scale((image.width() as f32, image.height() as f32).into())
+                Transform::scale((image.width() as f32, image.height() as f32).into()),
             );
             Some((image, sprite))
         }
@@ -29,7 +30,17 @@ impl Game for Outliner {
         data.set_resizable(true);
         data.set_title("Outliner");
 
+        let background_id = data.graphics.load_image(
+            &image::load_from_memory(include_bytes!("background.png")).unwrap(),
+            sprite::Filter::Nearest,
+        );
+
         Self {
+            background: Sprite::new_texture_rect_mapped(
+                data.graphics,
+                background_id,
+                RectTextureMap::from_grid((0.125, 0.125).into(), (0.0, 0.0).into()),
+            ).with_transform(Transform::scale(data.graphics.get_frame_size())),
             original_image: match std::env::args().nth(1) {
                 Some(path) => get_original_image(data, &path),
                 None => None,
@@ -38,6 +49,7 @@ impl Game for Outliner {
     }
 
     fn render<'a>(&'a mut self, mut frame: graphics::Frame<'a>) {
+        self.background.render_to(&mut frame);
         if let Some((_, sprite)) = &self.original_image {
             sprite.render_to(&mut frame);
         }
