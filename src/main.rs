db3db98fed51294866be65e0979ab180376dc486
SalsaGal <fielddashos@gmail.com>
2022-08-12 10:46:57 +1000

    Move loading image to seperate function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use current::sprite::{Sprite, Transform};
 use current::*;
-use image::{DynamicImage, GenericImageView};
+use image::DynamicImage;
 use winit::event::{Event, WindowEvent};
 
 fn main() {
@@ -9,30 +9,31 @@ fn main() {
 
 struct Outliner {
     original_image: Option<(DynamicImage, Sprite)>,
-    out_image: Option<(DynamicImage, Sprite)>,
+}
+
+fn get_original_image(data: &mut GameData, path: &str) -> Option<(DynamicImage, Sprite)> {
+    match image::open(path) {
+        Ok(image) => {
+            let id = data.graphics.load_image(&image, sprite::Filter::Nearest);
+            let sprite = Sprite::new_texture_rect(data.graphics, id).with_transform(
+                Transform::scale((image.width() as f32, image.height() as f32).into())
+            );
+            Some((image, sprite))
+        }
+        Err(_) => None,
+    }
 }
 
 impl Game for Outliner {
     fn init(data: &mut GameData) -> Self {
         data.set_resizable(true);
         data.set_title("Outliner");
-        data.graphics.frame_size = Some((2.0, 2.0).into());
-
-        let original_image = match std::env::args().nth(1) {
-            Some(path) => {
-                let image = image::open(path).unwrap();
-                let id = data.graphics.load_image(&image, sprite::Filter::Nearest);
-                let sprite = Sprite::new_texture_rect(data.graphics, id).with_transform(
-                    Transform::scale((1.0, 1.0).into()).with_translation((-0.5, 0.0, 0.0).into())
-                );
-                Some((image, sprite))
-            }
-            None => None,
-        };
 
         Self {
-            original_image,
-            out_image: None,
+            original_image: match std::env::args().nth(1) {
+                Some(path) => get_original_image(data, &path),
+                None => None,
+            },
         }
     }
 
@@ -48,14 +49,7 @@ impl Game for Outliner {
             ..
         } = event
         {
-            match image::open(path) {
-                Ok(image) => {
-                    let id = data.graphics.load_image(&image, sprite::Filter::Nearest);
-                    let sprite = Sprite::new_texture_rect(data.graphics, id);
-                    self.original_image = Some((image, sprite));
-                }
-                Err(err) => eprintln!("Error: {err}"),
-            }
+            self.original_image = get_original_image(data, path.to_str().unwrap());
         }
     }
 }
